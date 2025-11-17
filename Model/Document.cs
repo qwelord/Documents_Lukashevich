f8b24140f68e33a9cbcfb4359a72a302482a4961
qwel0rd <secondofall22@rambler.ru>
2025-11-17 11:51:18 +0500

    Реализованные модели. Лукашевич

---
+++ Model/Document.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Documents_Lukashevich.Model
+{
+    public class Document
+    {
+        public int id { get; set; }
+        public string src { get; set; }
+        public string name { get; set; }
+        public string user { get; set; }
+        public int id_document { get; set; }
+        public DateTime date { get; set; }
+        public int status { get; set; }
+        public string vector { get; set; }
+    }
+}

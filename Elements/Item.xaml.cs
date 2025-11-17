e0d3d28e5696ae8d47b26345e6d1f73e7b926d92
qwel0rd <secondofall22@rambler.ru>
2025-11-17 12:16:17 +0500

    Реализованные пользовательские элементы управления. Лукашевич

---
+++ Elements/Item.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Documents_Lukashevich.Classes;
+using System;
+using System.Security.Cryptography;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Documents_Lukashevich.Elements
+{
+    public partial class Item : UserControl
+    {
+        DocumentContext Document;
+
+        public Item(DocumentContext Document)
+        {
+            InitializeComponent();
+            img.Source = new BitmapImage(new Uri(Document.src));
+            lName.Content = Document.name;
+            lUser.Content = "Ответственный: " + Document.user;
+            lCode.Content = "Код документа: " + Document.id_document;
+            lDate.Content = "Дата поступления: " + Document.date.ToString("dd.MM.yyyy");
+            lStatus.Content = Document.status == 0 ? "Статус: входящий" : "Статус: исходящий";
+            lDirect.Content = "Направление: " + Document.vector;
+            this.Document = Document;
+        }
+
+        private void EditDocument(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.frame.Navigate(new Pages.Add(Document));
+        }
+
+        private void DeleteDocument(object sender, RoutedEventArgs e)
+        {
+            Document.Delete();
+            MainWindow.init.AllDocuments = new DocumentContext().AllDocuments();
+            MainWindow.init.OpenPages(MainWindow.pages.main);
+        }
+    }
+}

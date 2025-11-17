950930765476e71f80a9699fd6501e00824e894c
qwel0rd <secondofall22@rambler.ru>
2025-11-17 11:55:42 +0500

    Реализованные интерфейсы. Лукашевич

---
+++ Interfaces/IDocument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Documents_Lukashevich;
+using System.Threading.Tasks;
+
+namespace Documents_Lukashevich.Interfaces
+{
+    public interface IDocument
+    {
+        void Save(bool Update = false);
+
+        List<Documents_Lukashevich.Classes.DocumentContext> AllDocuments();
+
+        void Delete();
+    }
+}
